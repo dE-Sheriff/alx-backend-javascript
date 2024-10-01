2e5d068cedc174fe753d0e33abbd065a890a605b
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-10-01 05:45:21 +0100

    complete on EM6

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function iterateThroughObject(reportWithIterator) {
+	// eslint-disable-next-line no-array-constructor
+	const allEmployees = Array();
+
+	for (const employee of reportWithIterator) {
+		allEmployees.push(employee);
+	}
+
+	return allEmployees.join(' | ');
+}
---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+	const budget = getBudgetObject(income, gdp, capita);
+	return {
+		...budget,
+		getIncomeInDollars: (income) => `$${income}`,
+		getIncomeInEuros: (income) => `${income} euros`,
+	};
+}
